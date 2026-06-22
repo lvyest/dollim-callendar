eaf6f7744a699f731711bcbefd4362abe1dfc176
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-06-22 10:52:00 +0900

    feat(contracts): 스키마 배럴 export

---
+++ packages/contracts/src/index.ts
@@ -0,0 +1,3 @@
+// 공개 진입점: zod 스키마 + 타입만 노출.
+// OpenAPI 레지스트리/생성기는 node 전용이라 ./openapi 서브패스로만 사용한다.
+export * from './schemas';
---
+++ packages/contracts/src/schemas/index.ts
@@ -0,0 +1,7 @@
+export * from './common';
+export * from './member';
+export * from './session';
+export * from './attendance';
+export * from './note';
+export * from './event';
+export * from './guestbook';
